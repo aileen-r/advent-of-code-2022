2fdcdee3df41a1c79566c3e52decdd97239d5b43
Aileen Rae <aileenlrae@gmail.com>
2022-12-01 22:28:03 +0000

    Day 1, part 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2022
+
+This year I'm trying to brush up on my Python skills, which have not been touched since my degree project in 2017.
---
+++ day-01/solve.py
@@ -0,0 +1,54 @@
+import sys
+
+
+def parse(puzzle_input):
+    """Parse input."""
+
+    elves = []
+    for elf in puzzle_input.split("\n\n"):
+        elves.append(list(map(lambda x: int(x), elf.split("\n"))))
+    
+    return elves
+
+
+def part1(data):
+    """Solve part 1."""
+
+    calories_carried = []
+    for carried_item_calories in data:
+        calories_carried.append(sum(carried_item_calories))
+
+    return max(calories_carried)
+
+
+def part2(data):
+    """Solve part 2."""
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+
+    data = parse(puzzle_input)
+    # print(*data)
+
+    solution1 = part1(data)
+
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+
+    path = "./test-input.txt" if (sys.argv[1] == "-test" if len(sys.argv) > 1 else None) else "./input.txt"
+
+    print(f"Running solution against '{path}'...")
+
+    with open(path, 'r') as f:
+        puzzle_input = f.read()
+
+    # print(puzzle_input)
+
+    solutions = solve(puzzle_input)
+
+    print("\n".join(str(solution) for solution in solutions))
